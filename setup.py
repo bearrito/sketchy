727c4513d7fe3bd15d4eb13408cc192e81da447a
bearrito <bstrausser@trafficland.com>
2015-01-18 21:53:25 -0500

    Add setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sketchy',
+  packages = ['sketchy'],
+  version = '0.1',
+  description = 'A library for sketches',
+  author = 'Barrett Strausser',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/bearrito/sketchy',
+  download_url = 'https://github.com/bearrito/sketchy/tarball/0.1',
+  keywords = ['sketch', 'bloom filter', 'probablistic'],
+  classifiers = [],
+)
